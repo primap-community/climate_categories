15b99e7fe792dba3e9ed4deae13f4b4215401699
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2021-03-09 11:44:54 +0100

    Bump version: 0.2.0 → 0.2.1

--- climate_categories/__init__.py
+++ climate_categories/__init__.py
@@ -5,7 +5,7 @@ names of categorizations. To access the example categorization `Excat`, simply u
 
 __author__ = """Mika Pflüger"""
 __email__ = "mika.pflueger@pik-potsdam.de"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 import pathlib
 import typing
