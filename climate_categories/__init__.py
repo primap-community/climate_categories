5c7aeb4a59f19a32c414ee81c16aca4bb8fe2ff1
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2021-03-16 19:46:43 +0100

    Release version 0.3.0.

--- climate_categories/__init__.py
+++ climate_categories/__init__.py
@@ -5,7 +5,7 @@ names of categorizations. To access the example categorization `Excat`, simply u
 
 __author__ = """Mika Pflüger"""
 __email__ = "mika.pflueger@pik-potsdam.de"
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 
 import importlib
 import importlib.resources
