578eb7ae45958ad166c74ea4d7fde4167285d207
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2021-06-16 19:13:46 +0200

    Add parsing of a CSV file into a conversions object supporting somewhat complex formulas.

---
+++ climate_categories/_conversions.py
@@ -0,0 +1,132 @@
+"""Classes to represent conversion rules between categorizations."""
+import csv
+import pathlib
+import typing
+
+import pyparsing
+
+
+class Conversion:
+    """Rules for conversion between two categorizations."""
+
+    # Parsing rules for simple formulas in the CSV
+    # Supported operators at the moment are plus and minus
+    _operator = pyparsing.Char("+") ^ pyparsing.Char("-")
+    _operator_factors = {"+": 1, "-": -1}
+    # alphanumeric category codes can be given directly, others have to be quoted
+    _category_code = pyparsing.Word(pyparsing.alphanums) ^ pyparsing.QuotedString(
+        quoteChar='"', escChar="\\"
+    )
+    _formula = (
+        pyparsing.StringStart()
+        + pyparsing.Optional(_operator("unary_op"))
+        + _category_code("category_code")
+        + pyparsing.ZeroOrMore(_operator("binary_op") + _category_code("category_code"))
+        + pyparsing.StringEnd()
+    )
+
+    def __init__(
+        self,
+        categorization_a: str,
+        categorization_b: str,
+        conversion_factors: typing.List[
+            typing.Tuple[typing.Dict[str, int], typing.Dict[str, int]]
+        ],
+    ):
+        self.categorization_a = categorization_a
+        self.categorization_b = categorization_b
+        self.conversion_factors = conversion_factors
+
+    @classmethod
+    def _parse_formula(cls, formula: str) -> typing.Dict[str, int]:
+        """Parse a formula into factors for categories.
+
+        Parameters
+        ----------
+        formula: str
+            Formula comprising category codes connected with + or - . Alphanumeric
+            category codes can be given directly, other category codes must be quoted
+            using double quotes.
+
+        Returns
+        -------
+        code_factors: dict
+            mapping of category codes to factors
+
+        Examples
+        --------
+        >>> Conversion._parse_formula("A + B")
+        {'A': 1, 'B': 1}
+        >>> Conversion._parse_formula("-A+B")
+        {'A': -1, 'B': 1}
+        >>> Conversion._parse_formula('"-asdf.#" + B')
+        {'-asdf.#': 1, 'B': 1}
+        >>> Conversion._parse_formula(" A  -  B")
+        {'A': 1, 'B': -1}
+        >>> Conversion._parse_formula("-A")
+        {'A': -1}
+        >>> Conversion._parse_formula('-A+B - "A"')
+        {'A': -2, 'B': 1}
+        >>> Conversion._parse_formula("-A-")
+        Traceback (most recent call last):
+        ...
+        ValueError: Could not parse: '-A-', error: Expected ...
+        >>> Conversion._parse_formula("")
+        Traceback (most recent call last):
+        ...
+        ValueError: Could not parse: '', error: Expected ...
+        """
+        try:
+            tokens = cls._formula.parseString(formula)
+        except pyparsing.ParseException as exc:
+            raise ValueError(
+                f"Could not parse: {formula!r}, error: {exc.msg},"
+                f" error at char {exc.loc}"
+            )
+        code_factors = {}
+        # first operator is implicitly a plus, have to handle it specially
+        if "unary_op" in tokens:
+            op = tokens.pop(0)
+        else:
+            op = "+"
+        code = tokens.pop(0)
+        code_factors[code] = cls._operator_factors[op]
+        while tokens:
+            op = tokens.pop(0)
+            code = tokens.pop(0)
+            if code in code_factors:
+                code_factors[code] += cls._operator_factors[op]
+            else:
+                code_factors[code] = cls._operator_factors[op]
+
+        return code_factors
+
+    @classmethod
+    def _from_csv(cls, fd: typing.TextIO) -> "Conversion":
+        conversion_factors = []
+        reader = csv.reader(fd, quoting=csv.QUOTE_NONE, escapechar="\\")
+        header = next(reader)
+        for i, row in enumerate(reader):
+            try:
+                conversion_factors.append(
+                    (cls._parse_formula(row[0]), cls._parse_formula(row[1]))
+                )
+            except ValueError as err:
+                raise ValueError(f"Error in line {i + 2}: {err}")
+
+        return cls(
+            categorization_a=header[0],
+            categorization_b=header[1],
+            conversion_factors=conversion_factors,
+        )
+
+    @classmethod
+    def from_csv(
+        cls, filepath: typing.Union[str, pathlib.Path, typing.TextIO]
+    ) -> "Conversion":
+        """Read conversion from comma-separated-values file."""
+        if not isinstance(filepath, (str, pathlib.Path)):
+            return cls._from_csv(filepath)
+        fp = pathlib.Path(filepath)
+        with fp.open(filepath, newline="") as fd:
+            return cls._from_csv(fd)
---
+++ climate_categories/tests/test_conversions.py
@@ -0,0 +1,39 @@
+"""Tests for _conversions"""
+
+import importlib
+import importlib.resources
+
+import pytest
+
+import climate_categories
+import climate_categories._conversions
+import climate_categories.tests
+import climate_categories.tests.data
+
+
+class TestConversions:
+    def test_good_csv(self):
+        fd = importlib.resources.open_text(
+            climate_categories.tests.data,
+            "good_conversion.csv",
+        )
+        conv = climate_categories._conversions.Conversion.from_csv(fd)
+        assert conv.categorization_a == "A"
+        assert conv.categorization_b == "B"
+        assert conv.conversion_factors == [
+            ({"asdf": 1, "fdsa": 1}, {"asdf": 1}),
+            ({"5": 1}, {"4": 1}),
+            ({"b": 1, "argl.5": 1, "c": 1}, {"D": 1}),
+            ({"b": 1, "argl,5": 1, "c": 1}, {"D": 1}),
+            ({"b": 1, 'argl"5': 1, "c": 1}, {"D": 1}),
+            ({"argl,5": 1}, {"D": 1}),
+            ({"+": 1, "-": 1}, {"-": 1}),
+        ]
+
+    def test_bad_csv(self):
+        fd = importlib.resources.open_text(
+            climate_categories.tests.data,
+            "bad_conversion.csv",
+        )
+        with pytest.raises(ValueError, match="line 4"):
+            climate_categories._conversions.Conversion.from_csv(fd)
