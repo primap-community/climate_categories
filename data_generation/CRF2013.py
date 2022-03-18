1ce9ccd5b11e6a1460a83e9ee565fd95c8857693
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2022-03-18 09:45:51 +0100

    Update data_generation/CRF2013.py
    Co-authored-by: Robert Gieseke <rob.g@web.de>

--- data_generation/CRF2013.py
+++ data_generation/CRF2013.py
@@ -573,7 +573,7 @@ def main():
     # will be one grouping per country that reports in option c
     # these will be added in the submission year specific
     # terminologies
-    ncats["3.B.2"] = {"title": "Sheep"}  # possibly subsectors in reporte data
+    ncats["3.B.2"] = {"title": "Sheep"}  # possibly subsectors in reported data
     ncats["3.B.3"] = {"title": "Swine"}  # possibly subsectors in reporte data
     ncats["3.B.4"] = {
         "title": "Other Livestock",
