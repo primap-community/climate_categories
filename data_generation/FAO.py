44fc910dc745ba87f5d035af118c086699a1b344
Daniel Busch <daniel.busch@climate-resource.com>
2025-04-01 10:21:22 +0200

    lulucf and ag for fao

--- data_generation/FAO.py
+++ data_generation/FAO.py
@@ -401,7 +401,7 @@ def main():
     categories["5"] = {
         "title": "Drained organic soils",
         "comment": "Drained organic soils",
-        "info": {"gases": "CO2"},
+        "info": {"gases": ["CO2", "N2O"]},
         "children": [["5.A", "5.B"]],
     }
 
@@ -649,6 +649,60 @@ def main():
                 "N2O",
             ],
         },
+    }
+
+    categories["M.5.CO2"] = {
+        "title": "CO2 emissions from drained organic soils",
+        "comment": "The split by gas is needed to build M.AG and M.LULUCF",
+        "info": {
+            "gases": [
+                "CO2",
+            ],
+        },
+    }
+
+    categories["M.5.N2O"] = {
+        "title": "N2O emissions from drained organic soils",
+        "comment": "The split by gas is needed to build M.AG and M.LULUCF",
+        "info": {
+            "gases": [
+                "N2O",
+            ],
+        },
+    }
+
+    categories["M.AG"] = (
+        {
+            "title": "Agriculture",
+            "comment": "Agricultural part of AFOLU as defined in FAOSTAT data explorer",
+            "children": [
+                [
+                    "M.5.N2O",  # N2O drained organic soils
+                    "1.B",  # synthetic fertilisers
+                    "M.3.MA",  # manure applied to soils
+                    "M.3.MP",  # manure left on pasture
+                    "M.1.CR",  # crop residues
+                    "M.1.BCR",  # burning crop residues
+                    "M.3.EF",  # enteric fermentation
+                    "6.B",  # savanna fires
+                    "1.A.2.c",  # rice cultivation
+                ]
+            ],
+        },
+    )
+
+    categories["M.LULUCF"] = {
+        "title": "Land Use, Land Use Change, and Forestry",
+        "comment": "LULUCF part of AFOLU as defined in FAOSTAT data explorer",
+        "children": [
+            [
+                "M.5.CO2",  # CO2 drained organic soils
+                "4.A"  # forest land
+                "4.B"  # net forest conversion
+                "6.A"  # fires in organic soils
+                "6.C",  # forest fires
+            ]
+        ],
     }
 
     spec["categories"] = categories
